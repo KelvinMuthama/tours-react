f48b492b0942f43f97f650b900ab5fe62c5efc63
Kelvin <kelvinkamau387@gmail.com>
2021-11-23 00:17:23 +0300

    Export jsx components

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+export { default as Banner } from "./banner/Banner";
+export { default as Blog } from "./blog/Blog";
+export { default as Book } from "./book/Book";
+export { default as Credit } from "./credit/Credit";
+export { default as Cta } from "./ctaCta";
+export { default as Destination } from "./destination/Destination";
+export { default as Footer } from "./footer/Footer";
+export { default as Gallery } from "./gallery/Gallery";
+export { default as Heading } from "./heading/Heading";
+export { default as Home } from "./home/Home";
+export { default as Menu } from "./menu/Menu";
+export { default as Navbar } from "./navbar/Navbar";
+export { default as Review } from "./review/Review";
+export { default as Service } from "./service/Service";
+export { default as Video } from "./video/Video";
