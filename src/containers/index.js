fbc509911289a57580830cbc47429d2f51c8d0fe
Kelvin <kelvinkamau387@gmail.com>
2021-11-23 00:17:48 +0300

    Export jsx containers

---
+++ src/containers/index.js
@@ -0,0 +1,8 @@
+export { default as About } from "./about/About";
+export { default as Blogs } from "./blogs/Blogs";
+export { default as Destinations } from "./destinations/Destinations";
+export { default as Footer } from "./footer/Footer";
+export { default as Galleries } from "./galleries/Galleries";
+export { default as Header } from "./header/Header";
+export { default as Reviews } from "./reviews/Reviews";
+export { default as Services } from "./services/Services";
