9bef22d4dcb1ccbb91114e84ee1790b51d2e511a
Kelvin <kelvinkamau387@gmail.com>
2021-11-23 22:20:29 +0300

    Create navbar with styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,30 @@
+import React, { useState } from "react";
+import { Menu, Logo } from "..";
+import "./navbar.css";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [active, setActive] = useState(false);
+
+  const ToggleMenu = () => {
+    active ? setActive(false) : setActive(true);
+  };
+
+  return (
+    <>
+      <nav onClick={ToggleMenu}>
+        <Menu />
+      </nav>
+      <Logo />
+      <nav className={active ? "navbar active" : "navbar"}>
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#destination">destination</a>
+        <a href="#services">services</a>
+        <a href="#gallery">gallery</a>
+        <a href="#blogs">blogs</a>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
