6d606a0183eb18c124acbfcd35584b6873a521fb
Kelvin <kelvinkamau387@gmail.com>
2021-11-23 22:19:55 +0300

    Create menu button for small screens

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,5 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "./menu.css";
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div id="menu-btn">
+      <FontAwesomeIcon icon={faBars} />
+    </div>
+  );
 };
 
 export default Menu;
