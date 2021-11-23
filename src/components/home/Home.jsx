fcace8cea73ee7b2a4443e6c9b08c7f214552672
Kelvin <kelvinkamau387@gmail.com>
2021-11-23 23:14:42 +0300

    Create jsx and styles for home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,21 @@
+import { Cta } from "..";
+import "./home.css";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home" id="home">
+      <div className="content">
+        <span>get ready for</span>
+        <h3>an exciting adventure</h3>
+        <p>
+          Whether you're looking for a relaxing excursion, like a stroll around
+          the lake, or an invigorating adventure, like backcountry camping, look
+          no further.
+        </p>
+        <Cta />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
