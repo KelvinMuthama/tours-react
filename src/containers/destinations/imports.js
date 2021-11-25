cc37d093c1a53295ecb3d090034e93da79e41032
Kelvin <kelvinkamau387@gmail.com>
2021-11-26 01:26:00 +0300

    Import images for destination section

---
+++ src/containers/destinations/imports.js
@@ -0,0 +1,10 @@
+import des01 from "../../assets/des-1.jpg";
+import des02 from "../../assets/des-2.jpg";
+import des03 from "../../assets/des-3.jpg";
+import des04 from "../../assets/des-4.jpg";
+import des05 from "../../assets/des-5.jpg";
+import des06 from "../../assets/des-6.jpg";
+import des07 from "../../assets/des-7.jpg";
+import des08 from "../../assets/des-8.jpg";
+
+export { des01, des02, des03, des04, des05, des06, des07, des08 };
