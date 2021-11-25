86f937cb47ea95e9349c6a56e24b09e4042951c8
Kelvin <kelvinkamau387@gmail.com>
2021-11-25 23:28:53 +0300

    Add about jsx

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,5 +1,22 @@
-const About = () => {
-  return <div>About</div>;
-};
+import { Video } from "../../components";
+import { Cta } from "../../components";
+import "./about.css";
+
+const About = () => (
+  <section className="about" id="about">
+    <Video />
+    <div className="content">
+      <span>why choose us?</span>
+      <h3>Nature's Majesty Awaits You</h3>
+      <p>
+        Spending time in nature can improve memory, lower stress hormones, and
+        reduce feelings of depression or anxiety, among other perks. Being in
+        nature can help clear your head and bring you into the present moment,
+        and is a great way to de-stress and relax.
+      </p>
+      <Cta hrefSrc="#book-form" />
+    </div>
+  </section>
+);
 
 export default About;
