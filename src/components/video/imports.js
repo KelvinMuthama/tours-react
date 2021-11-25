3b185a06e1a87057ba17d8a7b82f7b719c6382c5
Kelvin <kelvinkamau387@gmail.com>
2021-11-25 15:13:39 +0300

    Create js file to export videos

---
+++ src/components/video/imports.js
@@ -0,0 +1,5 @@
+import vid01 from "../../assets/about-vid-1.mp4";
+import vid02 from "../../assets/about-vid-2.mp4";
+import vid03 from "../../assets/about-vid-3.mp4";
+
+export { vid01, vid02, vid03 };
