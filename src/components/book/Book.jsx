66ae2b63e8ddb6eddd40d7dd4e4d2c1b35401a35
Kelvin <kelvinkamau387@gmail.com>
2021-11-25 14:00:57 +0300

    Add jsx for the booking section

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,5 +1,29 @@
-const Book = () => {
-  return <div>Book</div>;
-};
+import "./book.css";
+
+const Book = () => (
+  <section className="book-form" id="book-form">
+    <form action="">
+      <div className="inputBox">
+        <span>where to?</span>
+        <input type="text" placeholder="place name" />
+      </div>
+      <div className="inputBox">
+        <span>when?</span>
+        <input type="date" />
+      </div>
+      <div className="inputBox">
+        <span>how many?</span>
+        <input type="number" placeholder="number of travelers" />
+      </div>
+      <input
+        data-aos="zoom-in"
+        data-aos-delay="600"
+        type="submit"
+        value="find now"
+        className="btn"
+      />
+    </form>
+  </section>
+);
 
 export default Book;
