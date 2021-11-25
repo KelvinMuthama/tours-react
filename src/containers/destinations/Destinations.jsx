56faff6ed6ed5278482ee778874bff487d7da0de
Kelvin <kelvinkamau387@gmail.com>
2021-11-26 01:25:02 +0300

    Add jsx for destinations

--- src/containers/destinations/Destinations.jsx
+++ src/containers/destinations/Destinations.jsx
@@ -1,5 +1,70 @@
-const Destinations = () => {
-  return <div>Destinations</div>;
-};
+import { Destination, Heading } from "../../components";
+import {
+  des01,
+  des02,
+  des03,
+  des04,
+  des05,
+  des06,
+  des07,
+  des08,
+} from "./imports";
+import "./destinations.css";
+
+const Destinations = () => (
+  <section className="destination" id="destination">
+    <Heading heading1="our destination" heading2="make your destination" />
+    <div className="box-container">
+      <Destination
+        heading="Tours & Travel"
+        content="While larger cities may boast impressive skylines, there is nothing
+            quite as beautiful as nature."
+        imgSrc={des01}
+      />
+      <Destination
+        heading="Tours & Travel"
+        content="While larger cities may boast impressive skylines, there is nothing
+            quite as beautiful as nature."
+        imgSrc={des02}
+      />
+      <Destination
+        heading="Tours & Travel"
+        content="While larger cities may boast impressive skylines, there is nothing
+            quite as beautiful as nature."
+        imgSrc={des03}
+      />
+      <Destination
+        heading="Tours & Travel"
+        content="While larger cities may boast impressive skylines, there is nothing
+            quite as beautiful as nature."
+        imgSrc={des04}
+      />
+      <Destination
+        heading="Tours & Travel"
+        content="While larger cities may boast impressive skylines, there is nothing
+            quite as beautiful as nature."
+        imgSrc={des05}
+      />
+      <Destination
+        heading="Tours & Travel"
+        content="While larger cities may boast impressive skylines, there is nothing
+            quite as beautiful as nature."
+        imgSrc={des06}
+      />
+      <Destination
+        heading="Tours & Travel"
+        content="While larger cities may boast impressive skylines, there is nothing
+            quite as beautiful as nature."
+        imgSrc={des07}
+      />
+      <Destination
+        heading="Tours & Travel"
+        content="While larger cities may boast impressive skylines, there is nothing
+            quite as beautiful as nature."
+        imgSrc={des08}
+      />
+    </div>
+  </section>
+);
 
 export default Destinations;
