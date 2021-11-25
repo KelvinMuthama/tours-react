111f032a457d671473df600c4a81a86b46a20818
Kelvin <kelvinkamau387@gmail.com>
2021-11-26 01:21:00 +0300

    Add jsx for destinations

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -1,5 +1,24 @@
-const Destination = () => {
-  return <div>Destination</div>;
-};
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import "./destination.css";
+
+const Destination = ({ heading, content, imgSrc }) => (
+  <div className="box">
+    <div className="image">
+      <img src={imgSrc} alt="tour" />
+    </div>
+    <div className="content">
+      <h3>{heading}</h3>
+      <p>{content}</p>
+
+      <a href="#destination">
+        read more
+        <i>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </i>
+      </a>
+    </div>
+  </div>
+);
 
 export default Destination;
