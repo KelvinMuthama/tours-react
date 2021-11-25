4d3a0ebcde9b9c18baaf81582a4f87f65bd59e7a
Kelvin <kelvinkamau387@gmail.com>
2021-11-25 23:31:00 +0300

    Add state to video component for onclick change

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -3,7 +3,22 @@ import { vid01, vid02, vid03 } from "./imports";
 import "./video.css";
 
 const Video = () => {
-  return <div>Video</div>;
+  const [videoSrc, setVideoSrc] = useState(vid01);
+
+  const nextVid1 = () => setVideoSrc(vid01);
+  const nextVid2 = () => setVideoSrc(vid02);
+  const nextVid3 = () => setVideoSrc(vid03);
+
+  return (
+    <div className="video-container">
+      <video src={videoSrc} muted autoPlay loop className="video"></video>
+      <div className="controls">
+        <span className="control-btn" onClick={nextVid1}></span>
+        <span className="control-btn" onClick={nextVid2}></span>
+        <span className="control-btn" onClick={nextVid3}></span>
+      </div>
+    </div>
+  );
 };
 
 export default Video;
