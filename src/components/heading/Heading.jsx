c90b414d029dc1d4c88de82c174efb81b1b5695b
Kelvin <kelvinkamau387@gmail.com>
2021-11-26 01:22:22 +0300

    Create heading with props

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -1,5 +1,10 @@
-const Heading = () => {
-  return <div>Heading</div>;
-};
+import "./heading.css";
+
+const Heading = ({ heading1, heading2 }) => (
+  <div className="heading">
+    <span>{heading1}</span>
+    <h1>{heading2}</h1>
+  </div>
+);
 
 export default Heading;
