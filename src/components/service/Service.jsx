b58a23d63f9f11bcd961c6d90700d574a3414e27
Kelvin <kelvinkamau387@gmail.com>
2021-11-26 13:47:46 +0300

    Create service jsx

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,5 +1,14 @@
-const Service = () => {
-  return <div>Service</div>;
-};
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./service.css";
+
+const Service = ({ heading, text, icon }) => (
+  <div className="box">
+    <i>
+      <FontAwesomeIcon icon={icon} />
+    </i>
+    <h3>{heading}</h3>
+    <p>{text}</p>
+  </div>
+);
 
 export default Service;
