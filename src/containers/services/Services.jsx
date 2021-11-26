280ea256fa85d0388c4c7c956d400b0cc9b631f4
Kelvin <kelvinkamau387@gmail.com>
2021-11-26 13:50:21 +0300

    Create services jsx

--- src/containers/services/Services.jsx
+++ src/containers/services/Services.jsx
@@ -1,5 +1,57 @@
-const Services = () => {
-  return <div>Services</div>;
-};
+import { Heading } from "../../components";
+import { Service } from "../../components";
+import {
+  faGlobe,
+  faHiking,
+  faWallet,
+  faHotel,
+  faUtensils,
+  faHeadset,
+} from "@fortawesome/free-solid-svg-icons";
+import "./services.css";
+
+const Services = () => (
+  <section className="services" id="services">
+    <Heading heading1="our services" heading2="countless experiences" />
+
+    <div className="box-container">
+      <Service
+        heading="worldwide"
+        text="We are always proud of being an experienced tours and travel services provider in the international market."
+        icon={faGlobe}
+      />
+
+      <Service
+        heading="adventures"
+        text="The body fuels the mind surrounded by the dynamic life systems that comprise the miracle of life."
+        icon={faHiking}
+      />
+
+      <Service
+        heading="food and drinks"
+        text="Before you head out on your hiking trip, start your day with a filling and healthy meal provided for you"
+        icon={faUtensils}
+      />
+
+      <Service
+        heading="affordable hotels"
+        text="Choose From a Wide Range of hotels. We Have Over 40 Payment Ways for Locking the Lowest Room Rates."
+        icon={faHotel}
+      />
+
+      <Service
+        heading="affordable price"
+        text="all our travel destinations are priced at affordable prices.We are programmed to believe that price and quality are interlinked"
+        icon={faWallet}
+      />
+
+      <Service
+        heading="24/7 support"
+        text="Whether for in-person assistance, technical support, or remote support, travel is here to ensure our clients are fulfilled in their pursuit for adventure"
+        icon={faHeadset}
+      />
+    </div>
+  </section>
+);
 
 export default Services;
