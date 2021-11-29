45dca95c17343006c42505d89d841b015d226d3b
Kelvin <kelvinkamau387@gmail.com>
2021-11-29 12:47:13 +0300

    Finish review section

--- src/components/review/Review.jsx
+++ src/components/review/Review.jsx
@@ -1,5 +1,16 @@
-const Review = () => {
-  return <div>Review</div>;
-};
+import "./review.css";
+
+const Review = ({ text, imgSrc, imgDesc, name, career }) => (
+  <div class="box">
+    <p>{text}</p>
+    <div class="user">
+      <img src={imgSrc} alt={imgDesc} />
+      <div class="info">
+        <h3>{name}</h3>
+        <span>{career}</span>
+      </div>
+    </div>
+  </div>
+);
 
 export default Review;
