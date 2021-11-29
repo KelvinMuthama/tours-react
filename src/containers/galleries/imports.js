ea190da3d88870bca8c5beda720d2e650581bfad
Kelvin <kelvinkamau387@gmail.com>
2021-11-29 11:02:22 +0300

    Import gallery images

---
+++ src/containers/galleries/imports.js
@@ -0,0 +1,11 @@
+import gal01 from "../../assets/gallery-img-1.jpg";
+import gal02 from "../../assets/gallery-img-2.jpg";
+import gal03 from "../../assets/gallery-img-3.jpg";
+import gal04 from "../../assets/gallery-img-4.jpg";
+import gal05 from "../../assets/gallery-img-5.jpg";
+import gal06 from "../../assets/gallery-img-6.jpg";
+import gal07 from "../../assets/gallery-img-7.jpg";
+import gal08 from "../../assets/gallery-img-8.jpg";
+import gal09 from "../../assets/gallery-img-9.jpg";
+
+export { gal01, gal02, gal03, gal04, gal05, gal06, gal07, gal08, gal09 };
