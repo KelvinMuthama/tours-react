554784a722d09e58dcf002738b38e2fd6a11958f
Kelvin <kelvinkamau387@gmail.com>
2021-11-29 11:02:57 +0300

    Create gallery component with props

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,5 +1,11 @@
-const Gallery = () => {
-  return <div>Gallery</div>;
-};
+import "./gallery.css";
+
+const Gallery = ({ destination, imgSrc, imgDesc }) => (
+  <div className="box">
+    <img src={imgSrc} alt={imgDesc} />
+    <span>travel spot</span>
+    <h3>{destination}</h3>
+  </div>
+);
 
 export default Gallery;
